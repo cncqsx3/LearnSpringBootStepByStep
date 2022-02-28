13f4a0538bb28bc0d58c763cc96284ef2142c42a
cncqsx3 <495504033@qq.com>
2022-02-28 23:51:41 +0800

    spring boot 2.2 之后和之前的测试类编写方法不一致。

--- Demos/demo3/src/test/java/com/example/demo/DemoApplicationTests.java
+++ Demos/demo3/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,6 +1,9 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
+// https://www.zhangshengrong.com/p/ArXGbgzBNj/
+// spring boot2.2之后和之前的测试类编写方法不一致。
+//import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
