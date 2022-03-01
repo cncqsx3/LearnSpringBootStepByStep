39c215ecbe7a7ecf7dc485c1f67c1fc9765cdfd7
LF <lf@China.com>
2022-03-01 10:22:11 +0800

    编译、运行成功。

--- Demos/demo-3.2/src/main/java/com/example/demo/HelloController.java
+++ Demos/demo-3.2/src/main/java/com/example/demo/HelloController.java
@@ -2,11 +2,20 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
 
 @Controller
 public class HelloController {
 	@GetMapping("/hello")
 	public String getHello() {
 		return "hello";
+	}
+
+	@PostMapping("/hello")
+	public String postRequest(@RequestParam("text1") String str, Model model) {
+		model.addAttribute("sample", str);
+		return "hello/response";
 	}
 }
