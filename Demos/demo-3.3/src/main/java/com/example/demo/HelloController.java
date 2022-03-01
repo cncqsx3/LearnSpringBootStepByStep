80084c950d9438b5ef9ace608a6667e90dbae14d
LF <lf@China.com>
2022-03-01 15:32:58 +0800

    demo-3.3 编译、运行成功。

--- Demos/demo-3.3/src/main/java/com/example/demo/HelloController.java
+++ Demos/demo-3.3/src/main/java/com/example/demo/HelloController.java
@@ -5,9 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Controller
 public class HelloController {
+	@Autowired
+	private HelloService service;
+
 	@GetMapping("/hello")
 	public String getHello() {
 		return "hello";
@@ -17,5 +21,12 @@ public class HelloController {
 	public String postRequest(@RequestParam("text1") String str, Model model) {
 		model.addAttribute("sample", str);
 		return "hello/response";
+	}
+
+	@PostMapping("/hello/db")
+	public String postDbRequest(@RequestParam("text2") String id, Model model) {
+		Employee employee = service.getEmployee(id);
+		model.addAttribute("employee", employee);
+		return "hello/db";
 	}
 }
